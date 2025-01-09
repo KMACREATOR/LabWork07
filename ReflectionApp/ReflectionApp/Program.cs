a537e0fd67a2d0176f2933208e81c9b08983eafc
KMACREATOR <148497147+KMACREATOR@users.noreply.github.com>
2025-01-09 04:19:57 +0300

    Add files via upload

---
+++ ReflectionApp/ReflectionApp/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Xml.Linq;
+using ReflectionLibrary;
+
+class Program
+{
+    static void Main()
+    {
+        string libraryPath = "ReflectionLibrary.dll";
+        Assembly assembly = Assembly.LoadFrom(libraryPath);
+
+        XElement root = new XElement("Classes");
+
+        foreach (Type type in assembly.GetTypes().Where(t => t.IsClass || t.IsAbstract))
+        {
+            XElement classElement = new XElement("Class",
+                new XAttribute("Name", type.Name));
+
+            var attribute = type.GetCustomAttribute<CommentAttribute>();
+            if (attribute != null)
+            {
+                classElement.Add(new XAttribute("Comment", attribute.Comment));
+            }
+
+            foreach (var property in type.GetProperties())
+            {
+                XElement propertyElement = new XElement("Property",
+                    new XAttribute("Name", property.Name),
+                    new XAttribute("Type", property.PropertyType.Name));
+                classElement.Add(propertyElement);
+            }
+
+            foreach (var method in type.GetMethods(BindingFlags.Public | BindingFlags.Instance | BindingFlags.DeclaredOnly))
+            {
+                XElement methodElement = new XElement("Method",
+                    new XAttribute("Name", method.Name),
+                    new XAttribute("ReturnType", method.ReturnType.Name));
+                classElement.Add(methodElement);
+            }
+
+            root.Add(classElement);
+        }
+
+        string xmlPath = "C:\\xml_handler\\goal.xml";
+        root.Save(xmlPath);
+        Console.WriteLine($"XML diagram saved to {xmlPath}");
+    }
+}
---
+++ ReflectionLibrary/ReflectionLibrary/AnimalClasses.cs
@@ -0,0 +1,100 @@
+﻿using System;
+
+namespace ReflectionLibrary
+{
+    public enum eClassificationAnimal
+    {
+        Herbivores,
+        Carnivores,
+        Omnivores
+    }
+
+    public enum eFavoriteFood
+    {
+        Meat,
+        Plants,
+        Everything
+    }
+
+    // Custom attribute
+    [AttributeUsage(AttributeTargets.Class, Inherited = false)]
+    public class CommentAttribute : Attribute
+    {
+        public string Comment { get; }
+
+        public CommentAttribute(string comment)
+        {
+            Comment = comment;
+        }
+    }
+
+    [Comment("Abstract class representing an animal")]
+    public abstract class Animal
+    {
+        public string Country { get; set; }
+        public bool HideFromOtherAnimals { get; set; }
+        public string Name { get; set; }
+        public eClassificationAnimal WhatAnimal { get; set; }
+
+        public abstract eFavoriteFood GetFavouriteFood();
+
+        public abstract void SayHello();
+    }
+
+    [Comment("Class representing a cow")]
+    public class Cow : Animal
+    {
+        public Cow()
+        {
+            WhatAnimal = eClassificationAnimal.Herbivores;
+        }
+
+        public override eFavoriteFood GetFavouriteFood()
+        {
+            return eFavoriteFood.Plants;
+        }
+
+        public override void SayHello()
+        {
+            Console.WriteLine("Moo!");
+        }
+    }
+
+    [Comment("Class representing a lion")]
+    public class Lion : Animal
+    {
+        public Lion()
+        {
+            WhatAnimal = eClassificationAnimal.Carnivores;
+        }
+
+        public override eFavoriteFood GetFavouriteFood()
+        {
+            return eFavoriteFood.Meat;
+        }
+
+        public override void SayHello()
+        {
+            Console.WriteLine("Roar!");
+        }
+    }
+
+    [Comment("Class representing a pig")]
+    public class Pig : Animal
+    {
+        public Pig()
+        {
+            WhatAnimal = eClassificationAnimal.Omnivores;
+        }
+
+        public override eFavoriteFood GetFavouriteFood()
+        {
+            return eFavoriteFood.Everything;
+        }
+
+        public override void SayHello()
+        {
+            Console.WriteLine("Oink!");
+        }
+    }
+}
